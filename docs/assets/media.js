8d4f0b4718b8c921f6ae75da3ed3e56159d1f98e
piyush <74035501+piyush-panpaliya@users.noreply.github.com>
2021-04-04 14:34:10 +0530

    Create media.js

---
+++ docs/assets/media.js
@@ -0,0 +1,7 @@
+function addCode() {
+            document.getElementById("s").innerHTML += 
+              "<script type="text/javascript" src="{{ site.baseurl }}/assets/scriptm.js">";
+        }
+
+if (document.documentElement.clientWidth < 900){
+    addcode();}
