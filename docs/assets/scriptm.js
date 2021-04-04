aef44a6a9fcf1e2728f696ced59a461ff0dc90cd
piyush <74035501+piyush-panpaliya@users.noreply.github.com>
2021-04-04 13:56:33 +0530

    Update scriptm.js

--- docs/assets/scriptm.js
+++ docs/assets/scriptm.js
@@ -141,8 +141,8 @@
     let width , height
     if (window.innerWidth <= window.innerHeight) {
         console.log(1)
-         width = window.innerWidth/5;
-         height = window.innerHeight/8;
+         width = window.innerWidth/3;
+         height = window.innerHeight/2;
     }
     else { width = window.innerWidth/3;
           height = window.innerHeight/1.5;}
