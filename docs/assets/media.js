c1a5cb1c2e87c7ebe3202820ace24402b203355d
piyush <74035501+piyush-panpaliya@users.noreply.github.com>
2021-04-04 14:38:34 +0530

    Update media.js

--- docs/assets/media.js
+++ docs/assets/media.js
@@ -1,7 +1,10 @@
-function addCode() {
-            document.getElementById("s").innerHTML += 
-              "<script type="text/javascript" src="{{ site.baseurl }}/assets/scriptm.js">";
+let code
+function addCode(code) {
+            document.getElementById("s").innerHTML += code ;
         }
 
 if (document.documentElement.clientWidth < 900){
-    addcode();}
+    addcode("<script type="text/javascript" src="{{ site.baseurl }}/assets/scriptm.js">");}
+else {
+      addcode("<script type="text/javascript" src="{{ site.baseurl }}/assets/script.js">");      
+}
